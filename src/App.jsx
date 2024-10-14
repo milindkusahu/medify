f1c1cdeca835ed61a0b1ea7c4d6f04a0b451b806
milindkusahu <milindsahu2000@gmail.com>
2024-10-14 11:31:26 +0530

    HospitalSearch component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import NavBar from "./components/NavBar/NavBar";
 import Footer from "./components/Footer/Footer";
+import Hero from "./components/Hero/Hero";
+import HospitalSearch from "./components/HospitalSearch/HospitalSearch";
 
 const App = () => {
   return (
     <div>
       <NavBar />
+      <Hero />
+      <HospitalSearch />
       <Footer />
     </div>
   );
