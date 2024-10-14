f67b11ffab6575c0e73079a3a522b9e2306fe958
milindkusahu <milindsahu2000@gmail.com>
2024-10-14 11:22:51 +0530

    Footer component done

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import NavBar from "./components/NavBar/NavBar";
+import Footer from "./components/Footer/Footer";
 
 const App = () => {
   return (
     <div>
       <NavBar />
+      <Footer />
     </div>
   );
 };
